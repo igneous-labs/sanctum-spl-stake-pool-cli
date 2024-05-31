98bebd51bfbc0117062657d79e5633ad2990ae71
Gabriel Hicks <60371583+gabrielhicks@users.noreply.github.com>
2024-05-31 13:45:11 -0500

    add increase and decrease stake ixs

--- src/subcmd/mod.rs
+++ src/subcmd/mod.rs
@@ -3,31 +3,34 @@ use clap::Subcommand;
 mod create_pool;
 mod deposit_stake;
 mod list;
-mod set_staker;
 mod sync_pool;
 mod sync_validator_list;
 mod update;
 mod withdraw_stake;
+mod decrease_validator_stake;
+mod increase_validator_stake;
 
 pub use create_pool::*;
 pub use deposit_stake::*;
 pub use list::*;
-pub use set_staker::*;
 pub use sync_pool::*;
 pub use sync_validator_list::*;
 pub use update::*;
 pub use withdraw_stake::*;
+pub use decrease_validator_stake::*;
+pub use increase_validator_stake::*;
 
 #[derive(Debug, Subcommand)]
 pub enum Subcmd {
     CreatePool(CreatePoolArgs),
     DepositStake(DepositStakeArgs),
     List(ListArgs),
-    SetStaker(SetStakerArgs),
     SyncPool(SyncPoolArgs),
     SyncValidatorList(SyncValidatorListArgs),
     Update(UpdateArgs),
     WithdrawStake(WithdrawStakeArgs),
+    DecreaseValidatorStake(DecreaseValidatorStakeArgs),
+    IncreaseValidatorStake(IncreaseValidatorStakeArgs),
 }
 
 impl Subcmd {
@@ -36,11 +39,12 @@ impl Subcmd {
             Self::CreatePool(_) => CreatePoolArgs::run(args).await,
             Self::DepositStake(_) => DepositStakeArgs::run(args).await,
             Self::List(_) => ListArgs::run(args).await,
-            Self::SetStaker(_) => SetStakerArgs::run(args).await,
             Self::SyncPool(_) => SyncPoolArgs::run(args).await,
             Self::SyncValidatorList(_) => SyncValidatorListArgs::run(args).await,
             Self::Update(_) => UpdateArgs::run(args).await,
             Self::WithdrawStake(_) => WithdrawStakeArgs::run(args).await,
+            Self::DecreaseValidatorStake(_) => DecreaseValidatorStakeArgs::run(args).await,
+            Self::IncreaseValidatorStake(_) => IncreaseValidatorStakeArgs::run(args).await,
         }
     }
 }
